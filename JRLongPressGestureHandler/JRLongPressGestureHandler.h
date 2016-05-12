a6ce003fd27fbb861783a94627acd6133a22ef42
Jeremy Rea <jeremyrea@fastmail.com>
2016-05-11 20:02:46 -0400

    Initial Commit

---
+++ JRLongPressGestureHandler/JRLongPressGestureHandler.h
@@ -0,0 +1,19 @@
+//
+//  JRLongPressGestureHandler.h
+//  JRLongPressGestureHandler
+//
+//  Created by Jeremy Rea on 2016-05-11.
+//  Copyright © 2016 Jeremy Rea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JRLongPressGestureHandler.
+FOUNDATION_EXPORT double JRLongPressGestureHandlerVersionNumber;
+
+//! Project version string for JRLongPressGestureHandler.
+FOUNDATION_EXPORT const unsigned char JRLongPressGestureHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JRLongPressGestureHandler/PublicHeader.h>
+
+
